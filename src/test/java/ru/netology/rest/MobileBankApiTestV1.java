18de69770f4877de51477725f2b15009245102bc
tatl9r <e.shuvaeva@outlook.com>
2022-08-02 21:49:09 +0300

    Modified test

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -6,6 +6,7 @@ import static io.restassured.RestAssured.given;
 
 class MobileBankApiTestV1 {
     @Test
+    
     void shouldReturnDemoAccounts() {
       // Given - When - Then
       // Предусловия
